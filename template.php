d90b562b0385dd67ce6717d9b8f379d4e8814b5e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-09-10 12:37:05 +0100

    template to all the new js

--- template.php
+++ template.php
@@ -15,6 +15,7 @@ function ms_bs_preprocess_page(&$vars) {
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
   drupal_add_js(drupal_get_path('theme', 'ms_bs').'/js/animatedcollapse.js');
+  drupal_get_path(drupal_add_js('theme', 'ms_bs').'/js/sticky-scroll.js')
 }
 
 function ms_bs_theme(){
